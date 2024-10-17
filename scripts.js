b8d9176142c0095339204a470c8a4f03ca9f1838
Majo Guerrero <70866808+majo-sh@users.noreply.github.com>
2024-10-17 16:53:52 +0000

    styles and active for leguage switcher

--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
-document.getElementById('es-btn').addEventListener('click', function() {
-    document.body.classList.add('es');
-    document.body.classList.remove('en');
+// Función para establecer el idioma
+function setLanguage(lang) {
+    document.body.classList.remove('es', 'en');
+    document.body.classList.add(lang);
+  
+
+    document.querySelectorAll('.language-switcher button').forEach(button => {
+      button.classList.remove('active');
+    });
+  
+   
+    if (lang === 'es') {
+      document.getElementById('es-btn').classList.add('active');
+    } else {
+      document.getElementById('en-btn').classList.add('active');
+    }
+  }
+  
+ 
+  document.getElementById('es-btn').addEventListener('click', function() {
+    setLanguage('es');
   });
   
   document.getElementById('en-btn').addEventListener('click', function() {
-    document.body.classList.add('en');
-    document.body.classList.remove('es');
+    setLanguage('en');
   });
+  
+ 
+  window.addEventListener('DOMContentLoaded', (event) => {
+    setLanguage('es'); 
+  });
+  
